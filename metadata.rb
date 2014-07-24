51e14b706e3a5b575b403e3aeb45984bb9a5c9dd
Nick LaMuro <nicklamuro@gmail.com>
2014-07-24 14:38:40 -0500

    v0.0.4

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name        "opsworks-bashrc"
 description "Bash functions and helpers for opsworks"
 maintainer  "Sport Ngin"
 license     "MIT"
-version     "0.0.3"
+version     "0.0.4"
 
 recipe "opsworks-bashrc", "Setups of the custom_bashrc.sh script for the app"

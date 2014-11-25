1ab229fd17f12456142f21a4a29e0df6ff48c818
Nick LaMuro <nicklamuro@gmail.com>
2014-11-25 17:48:16 -0600

    Adds basic kitchen/berkshelf setup

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'fog', '~>1.24.0'
+gem 'fog-core', '~>1.24.0'
+gem 'berkshelf', '~>2'
+gem 'test-kitchen'
+gem 'kitchen-ec2'
+gem 'serverspec'
+
+group :development do
+  gem 'kitchen-vagrant'
+end
---
+++ support/env.rb
@@ -0,0 +1,8 @@
+# AWS credentials from a local file
+if fog_file = File.expand_path('~/.fog') and File.exists?(fog_file)
+  fog_file = YAML.load_file(fog_file)
+  ENV['AWS_ACCESS_KEY_ID'] ||= fog_file.fetch('travis-ci', {})['aws_access_key_id']
+  ENV['AWS_SECRET_ACCESS_KEY'] ||= fog_file.fetch('travis-ci', {})['aws_secret_access_key']
+  ENV['AWS_KEYPAIR_NAME'] ||= fog_file.fetch('travis-ci', {})['aws_keypair_name']
+  ENV['EC2_SSH_KEY_PATH'] ||= 'ec2.pem'
+end

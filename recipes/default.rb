44e67f7bfa50a9c15830c2ac3ebd62892bdf6a10
Nick LaMuro <nicklamuro@gmail.com>
2014-12-04 15:22:53 -0600

    Adds ability to source custom files

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,21 @@
 Chef::Resource::Template.send(:include, OpsworksBashrc::HelperMethods)
 
+node.fetch(:opsworks_bashrc, {}).fetch(:custom_source_files, {}).each do |cookbook, file_name|
+  directory "/usr/share/custom_bashrc" do
+    owner 'root'
+    group 'root'
+    mode 0755
+  end
+
+  cookbook_file "/usr/share/custom_bashrc/#{file_name}" do
+    source file_name
+    cookbook cookbook
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
+end
+
 template "/etc/profile.d/custom_bashrc.sh" do
   source "bashrc.erb"
   owner "root"
@@ -15,6 +31,7 @@ template "/etc/profile.d/custom_bashrc.sh" do
     :zone => node[:opsworks][:instance][:availability_zone],
     :custom_env_variables => node.fetch(:opsworks_bashrc, {}).fetch(:custom_env_variables, {}),
     :custom_bash_aliases => node.fetch(:opsworks_bashrc, {}).fetch(:custom_bash_aliases, {}),
-    :custom_bash_scripts => node.fetch(:opsworks_bashrc, {}).fetch(:custom_bash_scripts, [])
+    :custom_bash_scripts => node.fetch(:opsworks_bashrc, {}).fetch(:custom_bash_scripts, []),
+    :custom_source_files => node.fetch(:opsworks_bashrc, {}).fetch(:custom_source_files, {})
   })
 end

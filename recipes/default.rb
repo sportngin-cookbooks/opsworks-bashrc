ac7c55034c6872208bce9e10db4f4ecda10a1b40
Emma Sax <emma.sax4@gmail.com>
2017-11-14 15:05:25 -0600

    Move to a helper method and specify the instance that this is running on

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,26 +21,7 @@ template "/etc/profile.d/custom_bashrc.sh" do
   owner "root"
   group "root"
   mode 0644
-
-  if node[:opsworks]
-    settings = { layers: node[:opsworks][:instance][:layers],
-                 private_ip: node[:opsworks][:instance][:private_ip],
-                 hostname: node[:opsworks][:instance][:hostname],
-                 box_type: node[:opsworks][:instance][:instance_type],
-                 node_id: node[:opsworks][:instance][:aws_instance_id],
-                 zone: node[:opsworks][:instance][:availability_zone]
-               }
-  else
-    instance = search("aws_opsworks_instance").first
-
-    settings = { layers: instance['layer_ids'],
-                 private_ip: instance['private_ip'],
-                 hostname: instance['hostname'],
-                 box_type: instance['instance_type'],
-                 node_id: instance['instance_id'],
-                 zone: instance['availability_zone']
-               }
-  end
+  settings = app_settings
 
   variables({
     :appdir => app_dir,
